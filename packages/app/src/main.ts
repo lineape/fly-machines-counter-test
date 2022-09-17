12fd82809acc145e6682157b2d8a7868af1d45af
Eli Kellendonk <elikellendonk@gmail.com>
2022-09-16 22:54:21 -0600

    chore: Kill the process after 10 seconds of no requests

--- packages/app/src/main.ts
+++ packages/app/src/main.ts
@@ -3,11 +3,34 @@ import express from 'express';
 async function main(): Promise<void> {
   const app = express();
 
+  app.use(killProcessMiddleware(1000 * 10));
+
   app.get('/', (_, res) => res.send('Hello World!'));
 
   app.listen(3000, () =>
     console.log('Example app listening on http://127.0.0.1:3000!'),
   );
+}
+
+function killProcessMiddleware(duration: number): express.Handler {
+  const createTimeout = () =>
+    setTimeout(() => {
+      console.log(`Killing process after timeout of ${duration}ms`);
+
+      process.exit(0);
+    }, duration);
+
+  let timeout = createTimeout();
+
+  return (_req, _res, next) => {
+    console.log('Request Received, resetting timeout');
+
+    clearTimeout(timeout);
+
+    timeout = createTimeout();
+
+    next();
+  };
 }
 
 main().catch((error) => {
